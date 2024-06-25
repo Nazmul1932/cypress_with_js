e8df1a4dc4e6dcc8c24cc4c088ae568336099c8f
Nh <bsse1032@iit.du.ac.bd>
2024-06-25 16:00:27 +0600

    Improvement

--- cypress/e2e/TestsUserManagement/test_user_management.cy.js
+++ cypress/e2e/TestsUserManagement/test_user_management.cy.js
@@ -29,42 +29,42 @@ describe("User Management Tests", () => {
         user_management.clickSave();
         cy.wait(3000);
     })
-     // it("Test Filter Driver User", () => {
-    //     cy.log("Click Driver Users")
-    //     user_management.clickDriverUsers();
-    //     cy.wait(2000);
-    //     cy.url().should('include', '/user-management/driver-users');
-    //     cy.log("Select Company")
-    //     user_management.selectCompany("UNIPET Company Limited");
-    //     cy.log("Filter Driver user by Admin Done")
-    // })
-    // it("Test Status Change of Admin User", () => {
-    //     user_management.clickAdminUsersMenu();
-    //     user_management.clickLockIcons();
-    //     cy.wait(3000);
-    // })
-    // it("Test Deletion of Admin User", () => {
-    //     user_management.clickAdminUsersMenu();
-    //     user_management.clickDeleteIcon();
-    //     cy.wait(3000);
-    // })
-    // it("Test Add New Fleet User", () => {
-    //     user_management.clickAdminUsersMenu();
-    //     user_management.clickFleetUser();
-    //     user_management.clickAddNewBtn()
-    //     user_management.setFirstName(faker.name.firstName());
-    //     user_management.setLastName(faker.name.lastName());
-    //     user_management.setEmail(faker.internet.email());
-    //     user_management.setPrimaryPhoneNumber("11111185");
-    //     user_management.selectState();
-    //     cy.wait(2000);
-    //     user_management.selectCity();
-    //     user_management.setPrimaryAddress();
-    //     user_management.selectCustomer();
-    //     user_management.selectRole();
-    //     user_management.clickSave();
-    //     cy.wait(3000);
-    // })
+     it("Test Filter Driver User", () => {
+        cy.log("Click Driver Users")
+        user_management.clickDriverUsers();
+        cy.wait(2000);
+        cy.url().should('include', '/user-management/driver-users');
+        cy.log("Select Company")
+        user_management.selectCompany("UNIPET Company Limited");
+        cy.log("Filter Driver user by Admin Done")
+    })
+    it("Test Status Change of Admin User", () => {
+        user_management.clickAdminUsersMenu();
+        user_management.clickLockIcons();
+        cy.wait(3000);
+    })
+    it("Test Deletion of Admin User", () => {
+        user_management.clickAdminUsersMenu();
+        user_management.clickDeleteIcon();
+        cy.wait(3000);
+    })
+    it("Test Add New Fleet User", () => {
+        user_management.clickAdminUsersMenu();
+        user_management.clickFleetUser();
+        user_management.clickAddNewBtn()
+        user_management.setFirstName(faker.name.firstName());
+        user_management.setLastName(faker.name.lastName());
+        user_management.setEmail(faker.internet.email());
+        user_management.setPrimaryPhoneNumber("11111185");
+        user_management.selectState();
+        cy.wait(2000);
+        user_management.selectCity();
+        user_management.setPrimaryAddress();
+        user_management.selectCustomer();
+        user_management.selectRole();
+        user_management.clickSave();
+        cy.wait(3000);
+    })
     afterEach(()=>{
         user_management.clickLogout()
     })
